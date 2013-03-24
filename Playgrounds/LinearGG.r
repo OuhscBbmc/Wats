b25cd16f64f444e039e8cd66a8f0eaaadd200dfc
Will Beasley <wibeasley@hotmail.com>
2013-03-23 22:37:28 -0500

    Figure 2 transferred to ggplot2

--- Playgrounds/LinearGG.r
+++ Playgrounds/LinearGG.r
@@ -84,46 +84,50 @@ dsLabelsX$Label <- lubridate::year(dsLabelsX$X)
 dsBreak <- data.frame(X=changeMonth,XEnd=changeMonth, Y=5, YEnd=6.8, Label="Bombing Effect")
 
 LinearPlot <- function( showLine=TRUE, showSmoother=TRUE, showRibbon=TRUE, showYears=TRUE, labelBreak=TRUE ) { 
-  g <- ggplot(ds, aes(x=Date, y=BirthRate, color=StageID))
+  g <- ggplot(ds, aes(x=Date, y=BirthRate, color=StageID))#
   g <- g + geom_segment(data=dsBreak, aes(x=X, xend=XEnd, y=Y, yend=YEnd), color=colorAfter, size=3, alpha=.3)
   
-  if( labelBreak ) g <- g + geom_text(data=dsBreak, aes(x=X, y=YEnd, label=Label), color=colorAfter, vjust=-.5, alpha=.8, size=4)#6
+  if( labelBreak ) g <- g + geom_text(data=dsBreak, aes(x=X, y=YEnd, label=Label), color=colorAfter, vjust=-.5, alpha=.5, size=4)#6
   
-  g <- g + geom_line(data=dsFebruary, aes(y=Rolling), color=smoothedLinear)
+  g <- g + geom_line(data=dsFebruary, aes(y=Rolling), color=smoothedLinear, alpha=.5)
   g <- g + geom_point(data=dsFebruary, aes(y=Rolling), size=2, shape=3, color=smoothedLinear)
   
-  #g <- ggplot(ds, aes(x=MonthID, y=BirthRate, color=StageID))
   if( showRibbon ) g <- g + geom_ribbon(data=dsStage1, aes(ymin=RollingLower, ymax=RollingUpper), fill=bandColorBefore[2], color=NA )
   if( showRibbon ) g <- g + geom_ribbon(data=dsStage2, aes(ymin=RollingLower, ymax=RollingUpper), fill=bandColorAfter[2], color=NA )
   g <- g + geom_point(shape=1, alpha=.5)
-  if( showLine ) g <- g + geom_line()#size=1)
-  if( showSmoother) g <- g + geom_path(data=ds[!is.na(ds$Rolling), ], aes(y=Rolling))#, size=1)
+  if( showLine ) {
+    #g <- g + geom_line() #This produces blocky lines, b/c it's checking for color switches
+    g <- g + geom_line(data=dsStage1, color=colorBefore )
+    g <- g + geom_line(data=dsStage2, color=colorAfter )
+  }
+  if( showSmoother) {
+    #g <- g + geom_path(data=ds[!is.na(ds$Rolling), ], aes(y=Rolling)) #This produces blocky lines, b/c it's checking for color switches
+    g <- g + geom_line(data=dsStage1, aes(y=Rolling), color=colorBefore )
+    g <- g + geom_line(data=dsStage2, aes(y=Rolling), color=colorAfter )
+  }
   
   
   if( showYears ) 
-    g <- g + annotate("text", x=dsLabelsX$X, y=dsLabelsX$Y, color=dsLabelsX$Color, label=dsLabelsX$Label, vjust=-.5, size=4) #8
+    g <- g + annotate("text", x=dsLabelsX$X, y=dsLabelsX$Y, color=dsLabelsX$Color, label=dsLabelsX$Label, vjust=-.5, size=3) #8
   if( !showYears ) 
-    g <- g + annotate("text", x=dsLabelsX$X, y=dsLabelsX$Y, color=dsLabelsX$Color, label=dsLabelsX$Label, vjust=-.5, size=4, alpha=.3) #8
+    g <- g + annotate("text", x=dsLabelsX$X, y=dsLabelsX$Y, color=dsLabelsX$Color, label=dsLabelsX$Label, vjust=-.5, size=3, alpha=.3) #8
 
   g <- g + scale_x_date(breaks=dateLocations, labels=scales::date_format("%Y"))
   g <- g + scale_y_continuous(breaks=yAxisTicks)
   g <- g + scale_color_continuous(low=colorBefore, high=colorAfter, guide=FALSE)
-  
-  # g <- g + geom_vline(x=as.integer(changeMonth), color=colorAfter)
-  # g <- g + annotate("text", x=changeMonth, y=max(dsBreak$YEnd), color=colorAfter, label="Bombing Effect", vjust=-.1)
-  
+
   g <- g + theme_minimal()
   g <- g + theme(axis.title=element_text(color="gray60", size=9))
   g <- g + theme(axis.text.x=element_blank())
-  g <- g + theme(axis.text.y=element_text(color="gray70"))
-  #g <- g + theme(axis.ticks=element_blank())
-  g <- g + theme(axis.ticks.length = unit(0, "cm"))
+  g <- g + theme(axis.text.y=element_text(color="gray80"))
+  g <- g + theme(axis.ticks.length = unit(0, "cm")) #g <- g + theme(axis.ticks=element_blank())
   g <- g + theme(axis.ticks.margin = unit(.00001, "cm"))
-  g <- g + theme(panel.grid.minor.y=element_line(color="gray90"))
-  g <- g + theme(panel.grid.major=element_line(color="gray85"))  
+  g <- g + theme(panel.grid.minor.y=element_line(color="gray90", size=.1))
+  g <- g + theme(panel.grid.major=element_line(color="gray85", size=.15))  
   g <- g + theme(panel.margin = unit(c(0, 0, 0, 0), "cm"))
   g <- g + theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))
   g <- g + labs(x="", y="General Fertility Rate")
+  if( showRibbon ) g <- g + labs(x="Bands Mark the .25 and .75 quantile for the previous 12 months")
   g
 }
 
@@ -135,8 +139,8 @@ bottom <- LinearPlot(showLine=FALSE, labelBreak=FALSE) #Bottom Panel
 # ggsave(file.path(pathDirectoryOutput, "LinearGGTry1.pdf"))
 
 vpLayout <- function(x, y) { viewport(layout.pos.row=x, layout.pos.col=y) }
-# pdf(file.path(pathDirectoryOutput, "Fig2.pdf"), width=widthTotal, height=heightTotal)
-png(file.path(pathDirectoryOutput, "Fig2.png"), width=widthTotal, height=heightTotal, units="in", res=600)
+pdf(file.path(pathDirectoryOutput, "Fig2.pdf"), width=widthTotal, height=heightTotal)
+# png(file.path(pathDirectoryOutput, "Fig2.png"), width=widthTotal, height=heightTotal, units="in", res=600)
 grid.newpage()
 pushViewport(viewport(layout=grid.layout(3,1)))
 print(top, vp=vpLayout(1,1))
