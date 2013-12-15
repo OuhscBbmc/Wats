66050d3d1babccfb053004b5114f58243ed3b90f
Will Beasley <wibeasley@hotmail.com>
2013-12-14 21:06:51 -0600

    Added tests for Augment

---
+++ inst/tests/MasterTestRunner.R
@@ -0,0 +1,18 @@
+rm(list=ls(all=TRUE)) #Clear all the variables before starting a new run.
+
+require(testthat)
+require(devtools)
+
+directoryTests <- file.path(devtools::inst("Wats"), "tests")
+
+ClearMostVariables <- function( ) {
+  rm(list=ls(all=TRUE)[!(ls(all=TRUE) %in% c("ClearMostVariables", "directoryTests") )])
+}
+
+try(detach("package:Wats"), silent=TRUE)
+require(Wats)
+#?Wats
+
+ClearMostVariables()
+test_file(file.path(directoryTests, "test.Augment.R"))
+
---
+++ inst/tests/test.Augment.R
@@ -0,0 +1,37 @@
+require(testthat)
+
+filePathOutcomes <- file.path(path.package("Wats"), "extdata", "BirthRatesOk.txt")
+
+
+
+# changeMonth <- as.Date("1996-02-15") # as.Date(dateBombing + weeks(40))
+# dsLinear$StageID <- ifelse(dsLinear$Date < changeMonth, 1L, 2L)
+
+###########
+context("Augment")
+###########
+test_that("AugmentYearDataWithMonthResolution", {
+  dsBirthRate <- read.table(filePathOutcomes, header=TRUE, sep="\t", stringsAsFactors=F)
+  dsBirthRate$Date <- as.Date(dsBirthRate$Date)
+  
+  expectedColumnNames <- c(colnames(dsBirthRate), "CycleIndex", "ProportionThroughCycle")
+  actual <- Wats::AugmentYearDataWithMonthResolution(ds=dsBirthRate, dateName="Date")
+  
+  expect_equal(mean(actual$ProportionThroughCycle), expected=.5, label="The mean of the proportion should be 0.5.")
+  expect_equal(actual$MonthID, expected=seq_len(120), label="There should be 120 Month IDs")
+  expect_equal(actual$CycleIndex, expected=rep(0:9, each=12), label="There should be 120 Cycle Indicies.")
+  expect_equal(colnames(actual), expected=expectedColumnNames, label="Two columns should be added (ie, CycleIndex, ProportionThroughCycle).")
+})
+
+test_that("AugmentYearDataWithSecondResolution", {
+  dsBirthRate <- read.table(filePathOutcomes, header=TRUE, sep="\t", stringsAsFactors=F)
+  dsBirthRate$Date <- as.POSIXct(dsBirthRate$Date, tz="GMT")
+  
+  expectedColumnNames <- c(colnames(dsBirthRate), "CycleIndex", "ProportionThroughCycle")
+  actual <- Wats::AugmentYearDataWithSecondResolution(ds=dsBirthRate, dateName="Date")
+  
+#   expect_equal(mean(actual$ProportionThroughCycle), expected=.5, label="The mean of the proportion should be 0.5.")
+  expect_equal(actual$MonthID, expected=seq_len(120), label="There should be 120 Month IDs")
+  expect_equal(actual$CycleIndex, expected=rep(0:9, each=12), label="There should be 120 Cycle Indicies.")
+  expect_equal(colnames(actual), expected=expectedColumnNames, label="Two columns should be added (ie, CycleIndex, ProportionThroughCycle).")
+})
