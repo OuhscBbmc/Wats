30452cfbb16074f841e444395f8d793d62e9cf2c
Will Beasley <wibeasley@insightresults.com>
2013-12-22 14:26:06 -0600

    Accounts for different days in a month & year

--- UtilityScripts/CalculateGfr.R
+++ UtilityScripts/CalculateGfr.R
@@ -27,17 +27,34 @@ testit::assert("All left records should find a right record", all(dsCountyMonth$
 dsCountyMonth$Dummy <- NULL
 
 dsCountyMonth$Date <- as.Date(ISOdate(dsCountyMonth$Year, dsCountyMonth$Month, 15L))
+dsCountyMonth$DaysInMonth <- lubridate::days_in_month(dsCountyMonth$Date)
+dsCountyMonth$DaysInYear <- as.integer(365L + lubridate::leap_year(dsCountyMonth$Date))
 
 rm(dsCensus, dsBirthCount, inputPathCensusCountyMonth, inputPathBirthCountCountyMonth)
 
 ###################
 # Calculate GFR
-###################
-dsCountyMonth$BirthRate <- dsCountyMonth$BirthCount / dsCountyMonth$FecundPopulation * 1000L
+###################L
+dsCountyMonth$BirthRateMonthly <- dsCountyMonth$BirthCount / dsCountyMonth$FecundPopulation * 1000L
+
+dsCountyMonth$BirthRate <- dsCountyMonth$BirthRateMonthly * dsCountyMonth$DaysInYear / dsCountyMonth$DaysInMonth
+
+#For diagnostic purposes, use only the 1990 estimate.
+dsCountyMonth <- plyr::ddply(dsCountyMonth, .variables="Fips", transform,
+                             BirthRateUnadjustedFrom1990=BirthCount / FecundPopulation[1] * 1000L)
 
 # require(ggplot2)
 # ggplot(dsCountyMonth, aes(x=Date, y=BirthRate, color=factor(Fips))) + geom_line() + labs(title="Distributions of County Fertility")
 # ggplot(dsCountyMonth, aes(x=BirthRate, color=factor(Fips))) + geom_density()
+
+# filePathOutcomes <- file.path(devtools::inst(name="Wats"), "extdata", "BirthRatesOk.txt")
+# dsOld <- read.table(file=filePathOutcomes, header=TRUE, sep="\t", stringsAsFactors=F)
+# dsOld$Date <- as.Date(dsOld$Date) + days(15)
+# 
+# ggplot(dsCountyMonth[dsCountyMonth$Fips==40109, ], aes(x=Date, color=factor(Fips))) + 
+#   geom_line(aes(y=BirthRate), color="tomato") +
+#   geom_line(aes(y=BirthRateUnadjustedFrom1990), color="blue") +
+#   geom_line(mapping=aes(y=BirthRate), data=dsOld, color="green")
 
 ###################
 # Write to disk
