b535333473b09af88df9f3bb2b5b9907b34b2951
Will Beasley <wibeasley@insightresults.com>
2013-12-24 00:53:39 -0600

    Bumped to version 0.2-1

--- inst/doc/MbrFigures.R
+++ inst/doc/MbrFigures.R
@@ -7,7 +7,7 @@ opts_chunk$set(
     fig.width=6.5, 
     fig.height=1.6,
     fig.path='figure_rmd/',
-    dpi=100
+    dpi=200
 )
 
 # options(markdown.HTML.header = system.file("misc", "vignette.css", package = "knitr"))
@@ -20,13 +20,16 @@ options(width=120) #So the output is 50% wider than the default.
 ## ----Definitions---------------------------------------------------------
 library(Wats)
 library(grid)
+library(plyr) 
 library(ggplot2) 
 library(boot) 
 
 changeMonth <- as.Date("1996-02-15")
 
+vpLayout <- function(x, y) { viewport(layout.pos.row=x, layout.pos.col=y) }
+
 fullSpread <- function( scores) { 
-  return( c(min(scores), max(scores)) ) 
+  return( range(scores) ) #A new function isn't necessary.  It's defined just to be consistent.
 }
 hSpread <- function( scores) { 
   return( quantile(x=scores, probs=c(.25, .75)) ) 
@@ -37,7 +40,7 @@ seSpread <- function( scores) {
 bootSpread <- function( scores, conf=.66 ) {
   plugin <- function( d, i ) { mean(d[i]) }
 
-  dist <- boot(data=scores, plugin, R=999)
+  dist <- boot(data=scores, plugin, R=99) #999 for the publication
   ci <- boot.ci(dist, type = c("bca"), conf=conf)
   return( ci$bca[4:5] ) #The fourth & fifth elements correspond to the lower & upper bound.
 }
@@ -61,7 +64,7 @@ CartesianRolling(
 
 
 ## ----Figure2IndividualStylized-------------------------------------------
-fig2Theme <- ggplot2::theme(
+lightTheme <- ggplot2::theme(
   axis.title          = element_text(color="gray60", size=9),
   axis.text.x         = element_text(color="gray80", hjust=0),
   axis.text.y         = element_text(color="gray80"),
@@ -113,9 +116,9 @@ bottomPanel <- CartesianRolling(
   drawJaggedLine = FALSE
 )
 
-topPanel <- topPanel + xScale + fig2Theme #+ yScale + yExpand
-middlePanel <- middlePanel + xScale + fig2Theme #+ yScale + yExpand
-bottomPanel <- bottomPanel + xScaleBlank + fig2Theme #+ yScale + yExpand
+topPanel <- topPanel + xScale + lightTheme #+ yScale + yExpand
+middlePanel <- middlePanel + xScale + lightTheme #+ yScale + yExpand
+bottomPanel <- bottomPanel + xScaleBlank + lightTheme #+ yScale + yExpand
 
 
 ## ----Figure2Combined, fig.height=4.8-------------------------------------
@@ -148,7 +151,7 @@ print(cartesianPeriodicSimple) #Print isn't necessary, but it makes my intention
 # yScale <- scale_y_continuous(breaks=5:7)
 # yExpand <- expand_limits(y=c(5, 7))
 
-print(cartesianPeriodicSimple + xScale + fig2Theme) # + yScale + yExpand
+print(cartesianPeriodicSimple + xScale + lightTheme) # + yScale + yExpand
 
 
 ## ----Figure5Basic--------------------------------------------------------
@@ -167,7 +170,7 @@ print(cartesianPeriodic)
 
 
 ## ----Figure5Stylized-----------------------------------------------------
-cartesianPeriodic <- cartesianPeriodic + xScale + fig2Theme # + yScale + yExpand
+cartesianPeriodic <- cartesianPeriodic + xScale + lightTheme # + yScale + yExpand
 print(cartesianPeriodic)
 
 
@@ -177,7 +180,8 @@ portfolioPolar <- PolarizeCartesian(
   dsStageCycle = portfolioCartesian$dsStageCycle, 
   yName = "BirthRate", 
   stageIDName = "StageID", 
-  plottedPointCountPerCycle = 7200)
+  plottedPointCountPerCycle = 7200
+)
 
 windows.options(antialias = "cleartype")
 grid.newpage()
@@ -210,7 +214,6 @@ pushViewport(viewport(
   ), 
   gp = gpar(cex=1, fill=NA)
 ))
-vpLayout <- function(x, y) { viewport(layout.pos.row=x, layout.pos.col=y) }
 
 ## Create top left panel
 pushViewport(viewport(layout.pos.col=1, layout.pos.row=1))
@@ -239,6 +242,63 @@ upViewport()
 ## Create bottom panel
 pushViewport(viewport(layout.pos.col=1:2, layout.pos.row=2, gp=gpar(cex=1)))
 print(cartesianPeriodic, vp=vpLayout(x=1:2, y=2)) #Print across both columns of the bottom row.
+upViewport()
+
+
+## ----Figure8, fig.height=6.5---------------------------------------------
+dsLinearAll <- AugmentYearDataWithMonthResolution(dsLinear=CountyMonthBirthRate2005Version, dateName="Date")
+
+#Identify the average size of the fecund population
+plyr::ddply(dsLinearAll, "CountyName", summarize, Mean=mean(FecundPopulation))
+
+
+GraphCountyComparison <- function( rowLabel="", countyName="oklahoma", spreadFunction=hSpread, changeMonth=as.Date("1996-02-15") ) {
+  dsLinear <- CountyMonthBirthRate2005Version[CountyMonthBirthRate2005Version$CountyName==countyName, ]
+  dsLinear <- AugmentYearDataWithMonthResolution(dsLinear=dsLinear, dateName="Date")
+  portfolioCartesian <- AnnotateData(dsLinear, dvName="BirthRate", centerFunction=median, spreadFunction=spreadFunction)
+  portfolioPolar <- PolarizeCartesian(dsLinear=portfolioCartesian$dsLinear, dsStageCycle=portfolioCartesian$dsStageCycle, yName="BirthRate", stageIDName="StageID", plottedPointCountPerCycle=7200)
+  cartesianPeriodic <- CartesianPeriodic(portfolioCartesian$dsLinear, portfolioCartesian$dsPeriodic, xName="Date", yName="BirthRate", stageIDName="StageID", changePoints=changeMonth, changePointLabels=""  )
+  
+  pushViewport(viewport(
+    layout=grid.layout(nrow=1, ncol=3, respect=F, widths=unit(c(2,1,3), c("line", "null", "null"))), 
+    gp=gpar(cex=1, fill=NA)
+  ))
+  pushViewport(viewport(layout.pos.col=1))
+  grid.rect(gp=gpar(fill="gray90", col=NA))
+  grid.text(rowLabel, rot=90)
+  popViewport()
+  
+  pushViewport(viewport(layout.pos.col=2))
+  polarPeriodic <- PolarPeriodic(dsLinear=portfolioPolar$dsObservedPolar, dsStageCyclePolar=portfolioPolar$dsStageCyclePolar, drawObservedLine=FALSE, yName="Radius", stageIDName="StageID", originLabel=NULL)
+  popViewport()
+  
+  pushViewport(viewport(layout.pos.col=3))
+  print(cartesianPeriodic + xScale + lightTheme, vp=vpLayout(x=1, y=1))
+  popViewport()
+  popViewport() #Finish the row
+}
+
+counties <- c("rogers", "tulsa", "oklahoma", "cleveland", "comanche")
+
+grid.newpage()
+pushViewport(viewport(layout=grid.layout(nrow=length(counties), ncol=1), gp=gpar(cex=1, fill=NA)))
+for( i in seq_along(counties) ) {
+  pushViewport(viewport(layout.pos.col=1, layout.pos.row=i))
+  GraphCountyComparison(countyName=counties[i], rowLabel=counties[i])
+  popViewport()
+}
+popViewport()
+
+
+## ----Figure9, fig.height=6.5 * 4/5---------------------------------------
+spreads <- c("hSpread", "fullSpread", "seSpread", "bootSpread")
+grid.newpage()
+pushViewport(viewport(layout=grid.layout(nrow=length(spreads), ncol=1), gp=gpar(cex=1, fill=NA)))
+for( i in seq_along(spreads) ) {
+  pushViewport(viewport(layout.pos.col=1, layout.pos.row=i))
+  GraphCountyComparison(spreadFunction=get(spreads[i]), rowLabel=spreads[i])
+  upViewport()
+}
 upViewport()
 
 
