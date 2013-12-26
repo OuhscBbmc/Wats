d73c039062d7fd8c96a35fa5bb9e0b91dfee59b1
Will Beasley <wibeasley@insightresults.com>
2013-12-26 16:32:00 -0600

    Adding more to 2014 replication vignette

--- R/Wats-package.r
+++ R/Wats-package.r
@@ -6,10 +6,14 @@
 #' @description Wrap-around Time Series (WATS) Plots for Interrupted Time Series Designs with Seasonal Patterns
 
 #' @author 
-#'  \href{http://scholar.google.com/citations?user=ffsJTC0AAAAJ}{William Howard Beasley} (\href{http://find.ouhsc.edu/Faculty.aspx?FacultyID=1064}{University of Oklahoma Health Sciences Center}, 
-#'  \href{https://www.oumedicine.com/pediatrics}{Dept of Pediatrics}, \href{http://ouhsc.edu/bbmc/}{Biomedical and Behavioral Methodology Core})
+#'  \href{http://scholar.google.com/citations?user=ffsJTC0AAAAJ}{William Howard Beasley} --\href{http://find.ouhsc.edu/Faculty.aspx?FacultyID=1064}{University of Oklahoma Health Sciences Center}, 
+#'  \href{https://www.oumedicine.com/pediatrics}{Dept of Pediatrics}, Biomedical and Behavioral Methodology Core (\href{http://ouhsc.edu/bbmc/}{BBMC})
 #'  
-#'  \href{http://www.vanderbilt.edu/psychological_sciences/bio/joe-rodgers}{Joseph Lee Rodgers} (Vanderbilt University, Nashville)
+#'  
+#'  \href{http://www.vanderbilt.edu/psychological_sciences/bio/joe-rodgers}{Joseph Lee Rodgers} --Vanderbilt University, Nashville
+#'  
+#'  
+#'  \href{http://en.wikipedia.org/wiki/Justice_League}{Matthew Schuelke}, \href{http://en.wikipedia.org/wiki/Justice_League}{League of Justice}.
 #'  
 #' @references 
 #' Rodger, J.L., Beasley, W.H., and Schuelke, M. (in submission). 
@@ -19,5 +23,6 @@
 #' Rodgers, J. L., St. John, C. A. & Coleman R. (2005).  
 #' \href{http://www.ncbi.nlm.nih.gov/pubmed/16463916}{Did Fertility Go Up after the Oklahoma City Bombing?  An Analysis of Births in Metropolitan Counties in Oklahoma, 1990-1999.}  
 #' \emph{Demography, 42}, 675-692.
+#' 
 #' @keywords package
 NULL
