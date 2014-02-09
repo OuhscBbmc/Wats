3e3bd1386986ec3d02631bb07ca662930d6004b9
Will Beasley <wibeasley@hotmail.com>
2014-02-09 14:10:57 -0600

    updating the Reproduce file

--- UtilityScripts/InstallPackagesInDevelopment.R
+++
@@ -1,3 +0,0 @@
-rm(list=ls(all=TRUE))
-devtools::install_github("testthat", "hadley") #For Version 0.8
-devtools::install_github("staticdocs", "hadley") #Not available on CRAN currently
--- UtilityScripts/Reproduce.R
+++ UtilityScripts/Reproduce.R
@@ -1,43 +1,76 @@
 ###################################
 ### Reproducible Research
 ###################################
-# When executed by R, this file will manipulated the original data sources (Oklahoma vital statistics
+# When executed by R, this file will manipulate the original data sources (Oklahoma vital statistics
 # and US Census estimates) to produce a groomed dataset suitable for analysis and graphing.
 
-#Clear memory from previous runs, and load the necessary packages.
-rm(list=ls(all=TRUE))
-require(base)
-require(knitr)
-require(markdown)
-require(testit)
+###################################
+# Clear memory from previous runs
+base::rm(list=base::ls(all=TRUE))
 
-# The working directory has been set correctly.  Much of the code assumes the working directory is the repository's root directory.
-testit::assert("The working directory should be set to the root of the package/repository.", base::basename(getwd())=="Wats")
+###################################
+# Verify the working directory has been set correctly.  Much of the code assumes the working directory is the repository's root directory.
+if( base::basename(base::getwd()) != "Wats" ) {
+  base::stop("The working directory should be set to the root of the package/repository.  ",
+             "It's currently set to `", base::getwd(), "`.")
+}
+###################################
+# Install the necessary packages.
+pathInstallPackages <- "./UtilityScripts/InstallPackages.R"
+if( !file.exists(pathInstallPackages)) {
+  base::stop("The file `", pathInstallPackages, "` was not found.  Make sure the working directory is set to the root of the repository.")
+}
+base::source(pathInstallPackages, local=new.env()) 
+
+base::rm(pathInstallPackages)
+###################################
+# Load the necessary packages.
+base::require(base)
+base::require(knitr)
+base::require(markdown)
+base::require(testit)
+
+###################################
+# Verify the necessary path can be found.
+pathCensus199x <- base::paste0("./Datasets/CensusIntercensal/STCH-icen199", 0:9, ".txt")
+pathCensus200x <- "./Datasets/CensusIntercensal/CO-EST00INT-AGESEX-5YR.csv"
+pathCountyFips <- "./Datasets/CountyFipsCode.csv"
+
+pathManipulateCensus <- "./UtilityScripts/IsolateCensusPopsForGfr.R"
+pathCalculateGfr <- "./UtilityScripts/CalculateGfr.R"
+pathsReports <- base::file.path("./vignettes", c("MbrFigures.Rmd", "OkFertilityWithIntercensalEstimates.Rmd"))
 
 # Assert that the working directory has been set properly initial datasets can be found.  The 
-testit::assert("The 10 census files from 199x should exist.", base::file.exists(base::paste0("./Datasets/CensusIntercensal/STCH-icen199", 0:9, ".txt")))
-testit::assert("The 200x census file should exist.", base::file.exists("./Datasets/CensusIntercensal/CO-EST00INT-AGESEX-5YR.csv"))
-testit::assert("The county FIPS values should exist.", base::file.exists("./Datasets/CountyFipsCode.csv"))
+testit::assert("The 10 census files from 199x should exist.", base::file.exists(pathCensus199x))
+testit::assert("The 200x census file should exist.", base::file.exists(pathCensus200x))
+testit::assert("The county FIPS values should exist.", base::file.exists(pathCountyFips))
+
+testit::assert("The file that restructures the census data should exist.", base::file.exists(pathManipulateCensus))
+testit::assert("The file that calculates the GFR should exist.", base::file.exists(pathCalculateGfr))
+testit::assert("The knitr Rmd files should exist.", base::file.exists(pathsReports))
+
+###################################
+# Run the files that manipulate and analyze.
 
 # Execute code that restructures the Census data
-base::source("./UtilityScripts/IsolateCensusPopsForGfr.R")
+base::source(pathManipulateCensus, local=new.env())
 
-# Assert that the intermediate files exist (the two produced by `IsolateCensusPopsForGfr.R`)
+# Assert that the intermediate files exist (the two files produced by `IsolateCensusPopsForGfr.R`)
 testit::assert("The yearly records should exist.", base::file.exists("./Datasets/CensusIntercensal/CensusCountyYear.csv"))
 testit::assert("The monthly records should exist.", base::file.exists("./Datasets/CensusIntercensal/CensusCountyMonth.csv"))
 
 #Execute code that combines the census and birth count data.
-base::source("./UtilityScripts/CalculateGfr.R")
+base::source(pathCalculateGfr, local=new.env())
 
 # Verify that the two human readable datasets are present.
 testit::assert("The CSV for the 2005 Version should exist.", base::file.exists("./Datasets/CountyMonthBirthRate2005Version.csv"))
 testit::assert("The CSV for the 2014 Version should exist.", base::file.exists("./Datasets/CountyMonthBirthRate2014Version.csv"))
 
+###################################
 # Build the reports
-paths_report <- base::file.path("./vignettes", c("MbrFigures.Rmd", "OkFertilityWithIntercensalEstimates.Rmd"))
-for( path_rmd in paths_report ) {
-  path_md <- base::gsub(pattern=".Rmd$", replacement=".md", x=path_rmd)
-  path_html <- base::gsub(pattern=".Rmd$", replacement=".html", x=path_rmd)
-  knitr::knit(input=path_rmd, output=path_md)
-  markdown::markdownToHTML(file=path_md, output=path_html)
+for( pathRmd in pathsReports ) {
+  pathMd <- base::gsub(pattern=".Rmd$", replacement=".md", x=pathRmd)
+  pathHtml <- base::gsub(pattern=".Rmd$", replacement=".html", x=pathRmd)
+  knitr::knit(input=pathRmd, output=pathMd)
+  markdown::markdownToHTML(file=pathMd, output=pathHtml)
 }
