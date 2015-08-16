2d0a4f45bc99a777b0e9abb293c7fc123ca0f953
Will Beasley <wibeasley@hotmail.com>
2015-08-16 16:26:20 -0500

    Add cran-comments. Remove stringi in travis's GitHub packages

--- UtilityScripts/Refresh.R
+++ UtilityScripts/Refresh.R
@@ -17,15 +17,6 @@ devtools::build_vignettes()
 
 # staticdocs::build_package(package="Wats", base_path="./../", examples=FALSE)
 
-# system("R CMD build --resave-data .") #Then move it up one directory.
-# tarBallPattern <- "^Wats_.+\\.tar\\.gz$"
-# file.copy(from=list.files(pattern=tarBallPattern), to="../", overwrite=TRUE)
-# system(paste("R CMD check --as-cran", list.files(pattern=tarBallPattern, path="..//", full.names=TRUE)))
-# unlink(list.files(pattern=tarBallPattern))
-# unlink(list.files(pattern=tarBallPattern, path="..//", full.names=TRUE))
-# unlink("Wats.Rcheck", recursive=T)
-# system("R CMD check --as-cran D:/Projects/RDev/Wats/Wats_0.1-1.tar.gz")
-
 # devtools::build_win(version="R-devel") #CRAN submission policies encourage the development version
 devtools::revdep_check(pkg="Wats", recursive=TRUE)
 # devtools::release(check=FALSE) #Careful, the last question ultimately uploads it to CRAN, where you can't delete/reverse your decision.
---
+++ cran-comments.md
@@ -0,0 +1,9 @@
+## Test environments
+* Local Win8, R 3.2.2 patched
+* win-builder (version="R-devel"); http://win-builder.r-project.org/6m63wUFP9hI9
+* Travis CI, Ubuntu 12.04 LTS; https://travis-ci.org/OuhscBbmc/Wats
+* AppVeyor, Windows Server 2012; https://ci.appveyor.com/project/wibeasley/wats
+
+This submission updates & improves various small things.  Many changes are to keep up with CRAN policy changes since April 2014.  Please tell me if there's something else I should do for CRAN.
+
+-Will Beasley
