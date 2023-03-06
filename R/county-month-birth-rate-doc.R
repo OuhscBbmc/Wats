119a7244d8fce0156d13067c03485bce00025e32
Will Beasley <wibeasley@hotmail.com>
2023-03-06 09:06:30 -0600

    wrapping doc lines
    ref #22

--- R/county-month-birth-rate-doc.R
+++ R/county-month-birth-rate-doc.R
@@ -3,11 +3,14 @@
 #' @docType data
 #' @title Monthly Growth Fertility Rates (GFR) for 12 urban Oklahoma counties
 #' @description Monthly Growth Fertility Rates (GFR) for 12 urban counties in Oklahoma
-#' between January 1990 and December 1999.  The GFR is defined as the number of births divided
+#' between January 1990 and December 1999.
+#' The GFR is defined as the number of births divided
 #' by the number of females (ages 15-44), multiplied by 1,000.
 #'
-#' There are two datasets in this package that are almost identical.  The 2014 version is better suited for substantive researchers
-#' in the areas of fertility and traumatic cultural events.  The 2005 version recreates the 2005 article
+#' There are two datasets in this package that are almost identical.
+#' The 2014 version is better suited for substantive researchers
+#' in the areas of fertility and traumatic cultural events.
+#' The 2005 version recreates the 2005 article
 #' and, therefore is better suited for the graphical aims of the 2014 manuscript.
 #'
 #' The difference is that the 2005 version uses constant estimate for a county population
@@ -25,10 +28,13 @@
 #'    15 to 44. `numeric`}
 #'    \item{birth_count}{The number of births in a county for the given month.
 #'    `integer`}
-#'    \item{date}{The year and month of the record, with a date of the 15th. Centering the date within the month makes the value a little more representative and the graphs a little easier. `date`}
+#'    \item{date}{The year and month of the record, with a date of the 15th.
+#'    Centering the date within the month makes the value a little more representative
+#'    and the graphs a little easier. `date`}
 #'    \item{days_in_month}{The number of days in the specific month. `integer`}
 #'    \item{days_in_year}{The number of days in the specific years `integer`}
-#'    \item{stage_id}{The "Stage" of the month.  The pre-bombing records are "1" (accounting for 9 months of gestation); the post-bombing months are "2". `integer`}
+#'    \item{stage_id}{The "Stage" of the month.  The pre-bombing records are "1"
+#'    (accounting for 9 months of gestation); the post-bombing months are "2". `integer`}
 #'    \item{birth_rate}{The Growth Fertility Rate (GFR). `numeric`}
 #' }
 #' @details
@@ -47,16 +53,16 @@
 #'
 #' @author Will Beasley
 #' @references
-#' Rodgers, J. L., St. John, C. A. & Coleman R. (2005).
-#' [Did Fertility Go Up after the Oklahoma City Bombing?  An Analysis of Births in Metropolitan Counties in Oklahoma, 1990-1999.](https://pubmed.ncbi.nlm.nih.gov/16463916/)
-#' *Demography, 42*, 675-692.
+#' * Rodgers, J. L., St. John, C. A. & Coleman R. (2005).
+#'   [Did Fertility Go Up after the Oklahoma City Bombing?
+#'   An Analysis of Births in Metropolitan Counties in Oklahoma, 1990-1999.](https://pubmed.ncbi.nlm.nih.gov/16463916/)
+#'   *Demography, 42*, 675-692.
 #'
-#' [Intercensal estimates for 199x](https://www.census.gov/data/tables/time-series/demo/popest/intercensal-1990-2000-state-and-county-totals.html)
+#' * [Intercensal estimates for 199x](https://www.census.gov/data/tables/time-series/demo/popest/intercensal-1990-2000-state-and-county-totals.html)
 #'
-#' [Intercensal estimates for 200x](https://www.census.gov/data/datasets/time-series/demo/popest/intercensal-2000-2010-counties.html)
+#' * [Intercensal estimates for 200x](https://www.census.gov/data/datasets/time-series/demo/popest/intercensal-2000-2010-counties.html)
 #'
-#'
-#' * [US Census Intercensal Estimates](https://www.census.gov/programs-surveys/popest/technical-documentation/file-layouts.html)
+#' * DOcumentation: [US Census Intercensal Estimates](https://www.census.gov/programs-surveys/popest/technical-documentation/file-layouts.html)
 #' for [199x](https://www.census.gov/programs-surveys/popest/technical-documentation/file-layouts/1990-2000-intercensal.html)
 #' and [200x](https://www.census.gov/programs-surveys/popest/technical-documentation/file-layouts/2000-2010-intercensal.html).
 #'
