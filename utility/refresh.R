cfefb679738824b1ae845bd0075e119779f75ce4
Will Beasley <wibeasley@hotmail.com>
2023-03-05 21:47:26 -0600

    tweak formatting

--- utility/refresh.R
+++ utility/refresh.R
@@ -1,6 +1,6 @@
 rm(list = ls(all.names = TRUE))
 deviceType <- ifelse(R.version$os == "linux-gnu", "X11", "windows")
-options(device = deviceType) #http://support.rstudio.org/help/discussions/problems/80-error-in-function-only-one-rstudio-graphics-device-is-permitted
+options(device = deviceType) # http://support.rstudio.org/help/discussions/problems/80-error-in-function-only-one-rstudio-graphics-device-is-permitted
 
 spelling::spell_check_package()
 # spelling::update_wordlist()
@@ -9,7 +9,7 @@ lintr::lint_package()
 urlchecker::url_check(); urlchecker::url_update()
 
 devtools::document()
-devtools::check_man() #Should return NULL
+devtools::check_man() # Should return NULL
 devtools::clean_vignettes()
 devtools::build_vignettes()
 
@@ -35,8 +35,7 @@ pkgdown::clean_site()
 pkgdown::build_site(run_dont_run = FALSE)
 # system("R CMD Rd2pdf --no-preview --force --output=./documentation-peek.pdf ." )
 
-devtools::run_examples(); #dev.off() #This overwrites the NAMESPACE file too
-# devtools::run_examples(, "CountyMonthBirthRateDoc.Rd")
+devtools::run_examples(); # dev.off() # This overwrites the NAMESPACE file too
 test_results_checked <- devtools::test()
 
 # devtools::check(force_suggests = FALSE)
@@ -46,7 +45,7 @@ devtools::check( # Equivalent of R-hub
   remote    = TRUE,
   incoming  = TRUE
 )
-devtools::check_rhub(email="wibeasley@hotmail.com")
+devtools::check_rhub(email = "wibeasley@hotmail.com")
 # devtools::check_win_devel() # CRAN submission policies encourage the development version
 # revdepcheck::revdep_check(num_workers = 4)
-# devtools::release(check=FALSE) #Careful, the last question ultimately uploads it to CRAN, where you can't delete/reverse your decision.
+# devtools::release(check=FALSE) # Careful, the last question ultimately uploads it to CRAN, where you can't delete/reverse your decision.
